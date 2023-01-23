5c5a69fbf7b5639d0318e888b9d60f7d4fc033b2
And132s <105209805+And132s@users.noreply.github.com>
2023-01-23 15:32:43 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 | It can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['botToken'] = "";
+$config['botToken'] = "5605757833:AAGuLWqnR9A3zW_jTkTTSk2A7jrSccRBhx4";
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ $config['botToken'] = "";
 | ID can be obtained from https://telegram.dog/username_to_id_bot
 |
 */
-$config['adminID'] = "";
+$config['adminID'] = "5428236702";
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ $config['adminID'] = "";
 | ID can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['logsID'] =  "";
+$config['logsID'] =  "5428236702";
 
 /*
 |--------------------------------------------------------------------------
@@ -52,10 +52,10 @@ $config['timeZone'] =  "Asia/Kolkata";
 | Database to Store User Data
 |
 */
-$config['db']['hostname'] =  "localhost";
-$config['db']['username'] =  "root";
-$config['db']['password'] =  "";
-$config['db']['database'] =  "checkerbot";
+$config['db']['hostname'] =  "sql11.freesqldatabase.com";
+$config['db']['username'] =  "sql11592075";
+$config['db']['password'] =  "nLRSMCG23v";
+$config['db']['database'] =  "sql11592075";
 
 /*
 |--------------------------------------------------------------------------
@@ -66,7 +66,7 @@ $config['db']['database'] =  "checkerbot";
 | Value is in Seconds. "20" = 20seconds
 |
 */
-$config['anti_spam_timer'] =  "20";
+$config['anti_spam_timer'] =  "02";
 
 /*
 |--------------------------------------------------------------------------
@@ -78,5 +78,5 @@ $config['anti_spam_timer'] =  "20";
 | array('sk1','sk2','sk3')
 |
 */
-$config['sk_keys'] =  array('sk_live_69GKI0saLB8uIEnxzv8VTvRX');
+$config['sk_keys'] =  array('sk_live_1RXGiMaFbcPnfTD8uSI6LOvx,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,sk_live_wvlarlcqwL8IWEcEcrYVFy1w');
 ?>
